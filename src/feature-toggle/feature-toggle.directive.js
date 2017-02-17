0cf56f2f8a4188292ea0f7fb268f8613f2f63677
m.sassen@gmail.com <m.sassen@gmail.com>
2017-02-17 18:42:34 +0100

    comment logging, simplify

--- src/feature-toggle/feature-toggle.directive.js
+++ src/feature-toggle/feature-toggle.directive.js
@@ -16,21 +16,20 @@ export default function (featureToggle, $log) {
       $log.debug(featureState);
 
       if (featureState === 'on') {
-        $log.debug('%s is on', featureName);
+        // $log.debug('%s is on', featureName);
         childScope = scope.$new();
-        $transclude(childScope, featureEl => {
+        $transclude(featureEl => {
           element.after(featureEl).remove();
         });
       } else if (featureState === 'disabled') {
-        $log.debug('%s is disabled', featureName);
+        // $log.debug('%s is disabled', featureName);
         childScope = scope.$new();
-        $transclude(childScope, featureEl => {
-          $log.debug(featureEl);
+        $transclude(featureEl => {
           featureEl[0].disabled = true;
           element.after(featureEl).remove();
         });
       } else {
-        $log.debug('%s should be destroyed', featureName);
+        // $log.debug('%s should be destroyed', featureName);
         if (childScope) {
           childScope.$destroy();
           childScope = null;
--- src/feature-toggle/feature-toggle.provider.js
+++ src/feature-toggle/feature-toggle.provider.js
@@ -9,8 +9,7 @@ export default class FeatureToggleProvider {
   }
 
   /**
-   *
-   * @returns {{featureToggle: state}}
+   * @returns state [off, on, disabled]
    */
   $get() {
     return {
